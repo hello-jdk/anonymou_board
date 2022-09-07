35db40c1a18892c73c9852c451a28e2c2b227560
JDK <ehdrjf2062@gmail.com>
2022-09-07 12:54:16 +0900

    feat, chore: gitignore 설정, ERD 설정

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+const { DB } = require("../config/config");
+
+const sequelize = new Sequelize(DB.DATABASE, DB.USERNAME, DB.PASSWORD, {
+  host: DB.HOST,
+  dialect: "mysql",
+  //logging: false,
+});
+
+//테이블 데이터 정의
+const definePostModel = require("./postModel");
+const postModel = definePostModel(sequelize);
+
+module.exports = {
+  sequelize,
+  postModel,
+};
