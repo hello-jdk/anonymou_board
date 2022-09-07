90f9c8a52d7f518728076c8102fb69e7a381c799
JDK <ehdrjf2062@gmail.com>
2022-09-07 16:25:03 +0900

    feat: post delete API

--- post/postService.js
+++ post/postService.js
@@ -19,4 +19,16 @@ async function updatePost(post) {
   await postRepository.updatePost(post);
 }
 
-module.exports = { createPost, updatePost };
+async function deletePost(post) {
+  // 검증
+  const oldPost = await postRepository.findOnebyId(post.id);
+  if (!oldPost) throw Error("해당 글이 존재하지 않습니다.");
+
+  const passwordValid = await postUtil.passwordValid(post.user_pw, oldPost.user_pw);
+  if (!passwordValid) throw Error("비밀번호가 일치하지 않습니다.");
+
+  // 게시글 삭제
+  await postRepository.deletePost(post);
+}
+
+module.exports = { createPost, updatePost, deletePost };
