ecd43decaf52430dbb2c045175f39e8a88b3779d
JDK <ehdrjf2062@gmail.com>
2022-09-07 13:38:15 +0900

    feat: 비밀번호 암호화 추가

--- post/postService.js
+++ post/postService.js
@@ -1,6 +1,8 @@
 const postRepository = require("./postRepository");
+const postUtil = require("./postUtil");
 
 async function createPost(post) {
+  post.user_pw = await postUtil.passwordEncrypt(post.user_pw);
   await postRepository.createPost(post);
 }
 
